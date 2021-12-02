5fafd7b1efdc745607e6fc070c35f51ea607be99
hynesnd <hynesnd@gmail.com>
2021-12-02 15:50:03 +0000

    project setup

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  ScrollView,
+  Image,
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const someText = "The count is: ";
+  const [count, setCount] = useState(0);
+
+  const handlePress = () => {
+    setCount(count + 1);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text>Some text Hi</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+# Neil & Sam's spike repo for React Native.
+
+We followed [these instructions](https://reactnative.dev/docs/environment-setup) for setting up the development environment using the Expo CLI method
+
+App.js code is from [Here](https://reactnative.dev/docs/intro-react-native-components)
+
+[Core components docs](https://reactnative.dev/docs/intro-react-native-components)
